663ff79d03ea0134ec1f04880b306941a5eeec2a
Ruben de Vries <ruben@rubensayshi.com>
2017-02-18 11:45:54 +0100

    fix tests

--- test/parser.js
+++ test/parser.js
@@ -10,6 +10,7 @@ test('simple fight, 1 target, end of fight', function (t) {
     };
 
     var events = [
+        {timestamp: 0, type: 'combatantinfo', sourceID: 1, targetID: 1, gear: []},
         {timestamp: 0, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Rejuvenation', guid: 1}},
         {timestamp: 1 * 1000, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Lifebloom', guid: 2}}
     ];
@@ -17,14 +18,15 @@ test('simple fight, 1 target, end of fight', function (t) {
     var parser = new rdruidMastery.Parser(fight, 1, [{id: 1, name: 'P1'}, {id: 2, name: 'P2'}], events, []);
     parser.parse();
 
-    t.deepEqual(parser.masteryStacks, {
+    t.deepEqual(parser.result().masteryStacksTimeRaw, {
         1: 1000,
         2: 9000,
         3: 0,
         4: 0,
         5: 0,
         6: 0,
-        7: 0
+        7: 0,
+        8: 0
     });
 });
 
@@ -37,6 +39,7 @@ test('simple fight, 1 target, removebuff', function (t) {
     };
 
     var events = [
+        {timestamp: 0, type: 'combatantinfo', sourceID: 1, targetID: 1, gear: []},
         {timestamp: 0, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Rejuvenation', guid: 1}},
         {timestamp: 1 * 1000, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Lifebloom', guid: 2}},
         {timestamp: 5 * 1000, type: 'removebuff', sourceID: 1, targetID: 1, ability: {name: 'Rejuvenation', guid: 1}},
@@ -46,14 +49,15 @@ test('simple fight, 1 target, removebuff', function (t) {
     var parser = new rdruidMastery.Parser(fight, 1, [{id: 1, name: 'P1'}, {id: 2, name: 'P2'}], events, []);
     parser.parse();
 
-    t.deepEqual(parser.masteryStacks, {
+    t.deepEqual(parser.result().masteryStacksTimeRaw, {
         1: 3000,
         2: 4000,
         3: 0,
         4: 0,
         5: 0,
         6: 0,
-        7: 0
+        7: 0,
+        8: 0
     });
 });
 
@@ -66,6 +70,7 @@ test('simple fight, 2 targets, end of fight', function (t) {
     };
 
     var events = [
+        {timestamp: 0, type: 'combatantinfo', sourceID: 1, targetID: 1, gear: []},
         {timestamp: 0, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Rejuvenation', guid: 1}},
         {timestamp: 0, type: 'applybuff', sourceID: 1, targetID: 2, ability: {name: 'Rejuvenation', guid: 1}},
         {timestamp: 1 * 1000, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Lifebloom', guid: 2}},
@@ -75,14 +80,15 @@ test('simple fight, 2 targets, end of fight', function (t) {
     var parser = new rdruidMastery.Parser(fight, 1, [{id: 1, name: 'P1'}, {id: 2, name: 'P2'}], events, []);
     parser.parse();
 
-    t.deepEqual(parser.masteryStacks, {
+    t.deepEqual(parser.result().masteryStacksTimeRaw, {
         1: 2000,
         2: 18000,
         3: 0,
         4: 0,
         5: 0,
         6: 0,
-        7: 0
+        7: 0,
+        8: 0
     });
 });
 
@@ -95,6 +101,7 @@ test('simple fight, 2 targets, removebuff, end of fight', function (t) {
     };
 
     var events = [
+        {timestamp: 0, type: 'combatantinfo', sourceID: 1, targetID: 1, gear: []},
         {timestamp: 0, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Rejuvenation', guid: 1}},
         {timestamp: 0, type: 'applybuff', sourceID: 1, targetID: 2, ability: {name: 'Rejuvenation', guid: 1}},
         {timestamp: 1 * 1000, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Lifebloom', guid: 2}},
@@ -105,14 +112,15 @@ test('simple fight, 2 targets, removebuff, end of fight', function (t) {
     var parser = new rdruidMastery.Parser(fight, 1, [{id: 1, name: 'P1'}, {id: 2, name: 'P2'}], events, []);
     parser.parse();
 
-    t.deepEqual(parser.masteryStacks, {
+    t.deepEqual(parser.result().masteryStacksTimeRaw, {
         1: 7000,
         2: 13000,
         3: 0,
         4: 0,
         5: 0,
         6: 0,
-        7: 0
+        7: 0,
+        8: 0
     });
 });
 
@@ -125,6 +133,7 @@ test('simple fight, 2 druids, 2 targets, removebuff, end of fight', function (t)
     };
 
     var events = [
+        {timestamp: 0, type: 'combatantinfo', sourceID: 1, targetID: 1, gear: []},
         {timestamp: 0, type: 'applybuff', sourceID: 1, targetID: 1, ability: {name: 'Rejuvenation', guid: 1}},
         {timestamp: 0, type: 'applybuff', sourceID: 2, targetID: 1, ability: {name: 'Rejuvenation', guid: 1}},
         {timestamp: 0, type: 'applybuff', sourceID: 1, targetID: 2, ability: {name: 'Rejuvenation', guid: 1}},
@@ -140,13 +149,14 @@ test('simple fight, 2 druids, 2 targets, removebuff, end of fight', function (t)
     var parser = new rdruidMastery.Parser(fight, 1, [{id: 1, name: 'P1'}, {id: 2, name: 'P2'}], events, []);
     parser.parse();
 
-    t.deepEqual(parser.masteryStacks, {
+    t.deepEqual(parser.result().masteryStacksTimeRaw, {
         1: 7000,
         2: 13000,
         3: 0,
         4: 0,
         5: 0,
         6: 0,
-        7: 0
+        7: 0,
+        8: 0
     });
 });
